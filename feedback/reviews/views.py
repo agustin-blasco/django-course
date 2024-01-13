5b68ead9dfd746bc2ddb9aa0d2149d422fe8e8ff
Agustin Blasco <agustinrblasco@gmail.com>
2024-01-13 17:47:56 -0300

    .

--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -5,32 +5,19 @@ from django.shortcuts import render
 from django.views import View
 from django.views.generic.base import TemplateView
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import FormView, CreateView
 
 from .forms import ReviewForm
 from .models import Review
 
 # Create your views here.
 
-class ReviewView(View):
+class ReviewView(CreateView):
+    model = Review
+    form_class = ReviewForm
+    template_name = "reviews/review.html"
+    success_url = "/thank-you"
 
-    def get(self, request):
-        form = ReviewForm()
-
-        return render(request, "reviews/review.html", {
-            "form": form
-        })
-
-    def post(self, request):
-
-        form = ReviewForm(request.POST)
-
-        if form.is_valid():
-            form.save() # This can be saved, because this is a ModelForm object.
-            return HttpResponseRedirect("/thank-you")
-        
-        return render(request, "reviews/review.html", {
-            "form": form
-        })
 
 class ThankYouView(TemplateView):
     template_name = "reviews/thank_you.html"
@@ -40,20 +27,14 @@ class ThankYouView(TemplateView):
         context["message"] = "This Works!"
         return context
 
+
 class ReviewsListView(ListView):
     template_name = "reviews/review_list.html"
     model = Review
 
     context_object_name = "reviews"
 
-    #def get_queryset(self):
-    #    base_query = super().get_queryset()
-    #    filtered_values = base_query.filter(rating__gt=2)
-    #    return filtered_values
 
 class SingleReviewView(DetailView):
     template_name = "reviews/single_review.html"
     model = Review
-
-    
-    
